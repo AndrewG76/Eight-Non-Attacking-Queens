eef521047402764a5997abf6778cfc524eb451f6
Andrew Gravino <aggravino@cpp.edu>
2023-02-13 12:50:41 -0800

    Write initial plan for calculating decent placement

---
+++ 8 Non-Attacking Queens.py
@@ -0,0 +1,34 @@
+#Gravino, Andrew
+#CS4200, Assignment 1
+#2/22/23
+
+
+queen0 = [-1, -1]
+queen1 = [-1, -1]
+queen2 = [-1, -1]
+queen3 = [-1, -1]
+queen4 = [-1, -1]
+queen5 = [-1, -1]
+queen6 = [-1, -1]
+queen7 = [-1, -1]
+
+boardDisplay = [0, 0, 0, 0, 0, 0, 0, 0], #this will be used to draw out where the queens will be and what the board will look like
+[0, 0, 0, 0, 0, 0, 0, 0], 
+[0, 0, 0, 0, 0, 0, 0, 0], 
+[0, 0, 0, 0, 0, 0, 0, 0], 
+[0, 0, 0, 0, 0, 0, 0, 0], 
+[0, 0, 0, 0, 0, 0, 0, 0], 
+[0, 0, 0, 0, 0, 0, 0, 0], 
+[0, 0, 0, 0, 0, 0, 0, 0]
+
+               
+#we'll need a way to verify things on a coordinate basis if anything is detectable diagonally, horizontally, or vertically
+#perhaps that can be solved by an equation of rise over run?
+#if the piece is directly above or below, that means the slope is going to be positive or negative infinite being directly above or below
+#if the piece is diagonal, that means the slope is going to be 1 or -1
+#if the piece is horizontally to the sides, that means the slope is going to be 0
+#we can do a thing where a queen will check the slope of it against every queen so long as the other queens have a valid set of coordinate
+
+
+
+#a valid set of coordinates for checking when the queens are actually "placed" is by checking like if the coordinates are negative or positive since there is no negative in an 8 by 8 chess board, going from 0 to 7
