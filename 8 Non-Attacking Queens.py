245b6b9d0544bc1f319419589b6e1376be89fab7
Andrew Gravino <aggravino@cpp.edu>
2023-02-22 00:32:49 -0800

    Revamp idea behind verification after figuring out major flaw of plan without backtracking

--- 8 Non-Attacking Queens.py
+++ 8 Non-Attacking Queens.py
@@ -1,18 +1,9 @@
 #Gravino, Andrew
 #CS4200, Assignment 1
 #2/22/23
+import math
 
-
-queen0 = [-1, -1]
-queen1 = [-1, -1]
-queen2 = [-1, -1]
-queen3 = [-1, -1]
-queen4 = [-1, -1]
-queen5 = [-1, -1]
-queen6 = [-1, -1]
-queen7 = [-1, -1]
-
-boardDisplay = [[0, 0, 0, 0, 0, 0, 0, 0], #this will be used to draw out where the queens will be and what the board will look like
+boardDisplay = [[1, 0, 0, 0, 0, 0, 0, 0], #this will be used to draw out where the queens will be and what the board will look like
                 [0, 0, 0, 0, 0, 0, 0, 0], 
                 [0, 0, 0, 0, 0, 0, 0, 0], 
                 [0, 0, 0, 0, 0, 0, 0, 0], 
@@ -21,7 +12,7 @@ boardDisplay = [[0, 0, 0, 0, 0, 0, 0, 0], #this will be used to draw out where t
                 [0, 0, 0, 0, 0, 0, 0, 0], 
                 [0, 0, 0, 0, 0, 0, 0, 0]]
 
-temporaryListOfQueenPositions = [queen0, queen1, queen2, queen3, queen4, queen5, queen6, queen7] 
+temporaryListOfQueenPositions = [[0,0]] #the coordinates will be y, x because we go row by row and then by each column
 
 def displayBoard():
     #this will be used to print the final layout of how the board will look and we can use it generally to figure out what our list looks at any specific time
@@ -32,37 +23,42 @@ def addQueensPositionToList(): #function that lets us take note of all the posit
     yIterator = 0
     xIterator = 0 #initialize out iterators
     for row in boardDisplay:
-        yIterator += 1
-        xIterator = 0 #at every new row, the x goes back to 0 
+        xIterator = 0 #at every new row, the column iterator goes back to 0
         for col in row:
-            xIterator += 1
             if col == 1: #then look for the places on the board where the piece is equal to 1 as that is where a queen has been placed.
-                if temporaryListOfQueenPositions.__contains__([yIterator, xIterator]): #we check to see if it would be unique adding it into the list
-                    pass #if we notice the same tuple, then we don't add that new queen
-                temporaryListOfQueenPositions.append([yIterator, xIterator]) #to get here means that it was totally unique and now we will be adding it into the list
+                if temporaryListOfQueenPositions.__contains__([yIterator, xIterator]) == False: #we check to see if it would be unique adding it into the list
+                    temporaryListOfQueenPositions.append([yIterator, xIterator])
+            xIterator += 1 #at every column in the row, the xIterator goes up by 1
+        yIterator += 1 #once we're done iterating through each column in the row, the yIterator goes up by 1 to the next
     print(temporaryListOfQueenPositions)
 
-def checkAllDirections(x1, y1): #this is gonna be autocalled by the placeQueens function checking
-    print("the function was called")
+def checkAllDirections(y1, x1): #this is gonna be autocalled by the placeQueens function checking
+    #print("the function was called")
     for i in range(0, len(temporaryListOfQueenPositions)):
-        print(temporaryListOfQueenPositions[i])
-        x2 = temporaryListOfQueenPositions[i][0]
-        y2 = temporaryListOfQueenPositions[i][1]
-        print("we are checking the passed", x1, "and", y1, "against queen's", x2, "and", y2)
-        if x2 - x1 == 0 and x1 - x2 == 0: #we need this to stop it early or else we'll end up creating a division by zero error
+        #print(temporaryListOfQueenPositions[i])
+        y2 = temporaryListOfQueenPositions[i][0] #the y-coordinate is handled by the first value in the individual list
+        x2 = temporaryListOfQueenPositions[i][1] #the x-coordinate it handled by the second value in the individual list
+        #print("we are checking the passed", y1, "and", x1, "against queen's", y2, "and", x2)
+        if abs(x1 - x2) == 0 or abs(x2 - x1) == 0: #we need this to stop it early or else we'll end up creating a division by zero error
             #and worst that happens, this means that the other queen detected was directly above it which is why the slope isn't 0 but instead would be infinity upwards
-            print("false, vertically spotted") 
+            #print("false, vertically spotted") 
             return False
         else:
             mValue = (y2 - y1)/(x2 - x1)
-        print("here is their slope value m:", mValue)
         if(mValue == 1 or mValue == -1): #perfect diagonals have a slope value of -1 or 1
-            print("false, diagonally spotted")
+            #print("false, diagonally spotted")
             return False
-        if(mValue == 0): #then this one exists because if slope is 0, then that means it's horizontally related to it
-            print("false, horizontally spotted")
+        if(mValue == 0 or mValue == -0): #then this one exists because if slope is 0, then that means it's horizontally related to it
+            #print("false, horizontally spotted")
+            return False
+        if(x1 == x2):
+            #print("false, vertically spotted")
+            return False
+        if(y1 == y2):
+            #print("false, horizontally spotted")
             return False
     print("true")
+    #print("here is their slope value m:", mValue)
     return True #you can only get here if all the diagonals, verticals, and horizontals were checked and we see none of them had a slope of -1, 1, 0, or was about to divide by 0
 
 def placeQueens():
@@ -75,21 +71,11 @@ def placeQueens():
                 displayBoard()
     print("after running through every iteration, here's how our final board looks")
     displayBoard()
-                
 
+def checkNumberOfQueens():
+    if len(temporaryListOfQueenPositions) != 8:
+        temporaryListOfQueenPositions[len(temporaryListOfQueenPositions) - 1][0] += 1 #this grabs the last item in the list and pulls the y-coordinate from that respective queen's coordinates to try again
 
-    '''
-    for i in range(0, 7):
-        print("meow", i)
-
-    meow 0
-    meow 1
-    meow 2
-    meow 3
-    meow 4
-    meow 5
-    meow 6
-    '''
 def main():
     #print("Showing what the board currently looks like")
     #displayBoard()
@@ -100,7 +86,6 @@ def main():
     #addQueensPositionToList()
     #print(temporaryListOfQueenPositions)
     #print("Now we will check the directions to see if we're good")   
-
     placeQueens()
     
 
